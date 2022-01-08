4522437b51aa422455cb8b624819074f776bdce1
Adam <ats92@cox.net>
2022-01-08 10:47:55 -0800

    added typeDefs

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
 const User = require("./User");
 const Post = require("./Post");
 const Pet = require("./Pet");
-module.exports = { User, Post };
+module.exports = { User, Post, Pet };
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,64 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    posts: [Post]
+    pets: [Pet]
+    friends: [User]
+  }
+  type Post {
+    _id: ID
+    postText: String
+    createdAt: String
+    username: String
+    replies: [Reply]
+    tags: [String]
+    categories: [String]
+  }
+  type Pet {
+    _id: ID
+    name: String
+    username: String
+    age: Int
+    petType: String
+    breed: String
+    img: String
+  }
+  type Reply {
+    _id: ID
+    replyText: String
+    createdAt: String
+    username: String
+  }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    posts(username: String!): [Thought]
+    post(_id: ID!): Thought
+    pets: [Pet]
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addPost(postText: String!): Post
+    addPet(
+      name: String!
+      age: Int!
+      petType: String!
+      breed: String!
+      img: String!
+    ): Pet
+    addReply(postId: ID!, postText: String!): Post
+    addFriend(friendId: ID!): User
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+module.exports = typeDefs;
