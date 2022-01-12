3064c005ccfb63a2679bce3e2b9fcdaca900ce59
Adam <ats92@cox.net>
2022-01-12 09:53:08 -0800

    added component structure

---
+++ client/src/components/PetList/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PetList = () => {
+  return <div></div>;
+};
+
+export default PetList;
---
+++ client/src/components/ReplyForm/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReplyForm = () => {
+  return <div></div>;
+};
+
+export default ReplyForm;
