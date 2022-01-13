30e3b15d35122767e2bd14e1f0c2fcd834f205cf
Adam <ats92@cox.net>
2022-01-12 18:19:15 -0800

    added apollo client set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  creteHttpLink,
+} from "@apollo/client";
 
+const httpLInk = createHttpLink({
+  uri: "graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 function App() {
-  return <div></div>;
+  return (
+    <ApolloProvider client={client}>
+      <div></div>;
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
-const Login = () => {
+const Login = (props) => {
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
   return <div></div>;
 };
 
